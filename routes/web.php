1a9ed004ac66b9fc5a273202d0f1903ee7f4847d
msamgan <mohdsamgankhan@gmail.com>
2025-05-02 12:59:05 -0400

    update the auto route discovery.

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,8 @@
 
 declare(strict_types=1);
 
-use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', fn () => redirect('login'))->name('welcome');
-
 Route::get('dashboard', fn () => Inertia::render('Dashboard'))->middleware(['auth', 'verified'])->name('dashboard');
-
-$moduleRoutesDir = 'routes/modules/';
-foreach (File::allFiles(base_path($moduleRoutesDir)) as $file) {
-    require base_path($moduleRoutesDir).$file->getFilename();
-}
