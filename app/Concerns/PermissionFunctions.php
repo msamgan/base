6fd7eb9ad769b7d5c473f266e046beb5ad786f46
msamgan <mohdsamgankhan@gmail.com>
2025-05-04 11:50:29 -0400

    added permission trait

---
+++ app/Concerns/PermissionFunctions.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Concerns;
+
+trait PermissionFunctions
+{
+    public function makePermissionName(string $module, string $permission): string
+    {
+        return $module . '.' . $permission;
+    }
+}
